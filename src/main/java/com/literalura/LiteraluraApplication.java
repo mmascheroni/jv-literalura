23e7e45ecf9ab3913ba0a9bca73ebf82d08533ff
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-04-30 16:08:07 -0300

    Added processAllBook()

--- src/main/java/com/literalura/LiteraluraApplication.java
+++ src/main/java/com/literalura/LiteraluraApplication.java
@@ -1,20 +1,13 @@
 package com.literalura;
 
-import com.literalura.principal.ProcessBook;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-import java.security.Principal;
-
-@ComponentScan(basePackages = {"com.literalura"})
 @SpringBootApplication
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
-
 	}
 
 }
--- src/main/java/com/literalura/principal/ProcessBook.java
+++ src/main/java/com/literalura/principal/ProcessBook.java
@@ -1,9 +1,11 @@
 package com.literalura.principal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.literalura.entity.Author;
 import com.literalura.entity.Book;
-import com.literalura.entity.DataAuthor;
 import com.literalura.entity.DataBook;
 import com.literalura.service.AuthorService;
 import com.literalura.service.BookService;
@@ -13,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ProcessBook {
@@ -22,7 +26,7 @@ public class ProcessBook {
     @Autowired
     private AuthorService authorService;
 
-    @PostConstruct
+
     public void processBook() throws IOException, InterruptedException {
         ObjectMapper objectMapper = new ObjectMapper();
         APIConsume apiConsume = new APIConsume();
@@ -39,10 +43,55 @@ public class ProcessBook {
             Author authorSave = authorService.saveAuthor(author);
         }
 
-
         Book bookSave = bookService.saveBook(book);
 
         System.out.println("Book to save: " + bookSave);
+
+    }
+
+    @PostConstruct
+    public void processBookAllBooks() throws IOException, InterruptedException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        APIConsume apiConsume = new APIConsume();
+
+        String url = "https://gutendex.com/books/";
+        String jsonRes = apiConsume.getRequest(url);
+
+        List<DataBook> dataBooks = new ArrayList<>();
+
+        JsonElement jsonElement = JsonParser.parseString(jsonRes);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+
+        List<JsonElement> results = new ArrayList<>();
+
+        if (jsonObject.has("results")) {
+            JsonElement resultsElement = jsonObject.get("results");
+            if (resultsElement.isJsonArray()) {
+                for (JsonElement resultElement : resultsElement.getAsJsonArray()) {
+                    results.add(resultElement);
+                }
+            }
+        }
+
+        for (JsonElement element : results) {
+            try {
+                DataBook dataBook = objectMapper.readValue(element.toString(), DataBook.class);
+                dataBooks.add(dataBook);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        for ( DataBook dataBook : dataBooks ) {
+            Book book = objectMapper.convertValue(dataBook, Book.class);
+
+            for (Author author : book.getAuthors()) {
+                Author authorSave = authorService.saveAuthor(author);
+            }
+
+            Book bookSave = bookService.saveBook(book);
+        }
 
     }
 }
